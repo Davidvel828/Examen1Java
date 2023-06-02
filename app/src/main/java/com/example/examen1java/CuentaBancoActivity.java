df472e1e04472f9f1b8b777880c3dcc3cf71d2b5
Manuel Alexis Flores Prado <alexisprfl@gmail.com>
2023-06-02 13:49:38 -0600

    Creacion de proyecto

---
+++ app/src/main/java/com/example/examen1java/CuentaBancoActivity.java
@@ -0,0 +1,14 @@
+package com.example.examen1java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuentaBancoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_banco);
+    }
+}
